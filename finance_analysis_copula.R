568131450ca0946fc7bc29d3685a83a61ef48263
Sergi Vilardell <sergivilardell13@gmail.com>
2020-10-27 18:31:27 +0100

    log returns plot

--- finance_analysis_copula.R
+++ finance_analysis_copula.R
@@ -1,6 +1,7 @@
 
 # Libraries ---------------------------------------------------------------
 library(tidyverse)
+library(data.table)
 library(rfinance)
 
 
@@ -11,6 +12,9 @@ source("finance_analysis_functions.R")
 # Stock Data --------------------------------------------------------------
 msf <- rfinance::get_prices(symbol = 'MSFT')
 apl <- rfinance::get_prices(symbol = 'AAPL')
+
+# APPLE data has numbers coded as characters
+apl[, 2:7] <- sapply(apl[, 2:7], as.numeric)
 stock_data <- list(msf = msf, apl = apl)
 
 # Are starting and ending dates the same?
@@ -27,3 +31,29 @@ n_rows <- nrow(stock_data_filtered$apl)
 
 # This will be true if every date coincides
 sum(logic_check)/n_rows == 1
+
+# Get log-returns
+msf_logr <- stock_data_filtered$msf %>% 
+  mutate(log_returns = log(Close / Open)) %>% 
+  pull(log_returns)
+
+apl_logr <- stock_data_filtered$apl %>% 
+  mutate(log_returns = log(Close / Open)) %>% 
+  pull(log_returns)
+
+
+all_logr <- data.frame(MSF = msf_logr,
+                       APPLE = apl_logr) %>% 
+  melt(measure.vars = c("MSF", "APPLE"),
+       variable.name = "Company",
+       value.name = "LogReturn")
+
+all_logr %>% 
+  ggplot(aes(x = Company, y = LogReturn))+
+  #geom_boxplot(color = "gray60", outlier.alpha = 0) +
+  geom_jitter(size = 1, alpha = 0.1, width = 0.4)+
+  coord_flip()+
+  theme_bw()
+
+
+
